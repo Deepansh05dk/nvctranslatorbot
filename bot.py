502aa1021694aba52f8aa0808a4f386db8e09582
Deepansh05dk <84671261+Deepansh05dk@users.noreply.github.com>
2023-11-16 01:35:36 +0530

    added reply to any tweet functionality

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
-import sqlite3
+import re
 import tweepy
 from dotenv import load_dotenv
-from os import environ
+import os
 import requests
 import logging
 import time
-import urllib.parse
 from datetime import datetime, timedelta
+import json
 
 
 load_dotenv()
@@ -20,183 +20,159 @@ logging.basicConfig(
     force=True)
 
 # Config details of twitter API
-BEARER_TOKEN = environ["BEARER_TOKEN"]
-API_KEY = environ["CONSUMER_KEY"]
-API_KEY_SECRET = environ["CONSUMER_SECRET"]
-ACCESS_TOKEN = environ["ACCESS_TOKEN_KEY"]
-ACCESS_TOKEN_SECRET = environ["ACCESS_TOKEN_SECRET"]
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+API_KEY = os.getenv("CONSUMER_KEY")
+API_KEY_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN_KEY")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+last_processed_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+def get_last_processed_time():
+    global last_processed_time
+    return last_processed_time
 
 
-def create_connection(db_file):
-    """ Create a database connection to the SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        logger.info("Database successfully connected")
-    except sqlite3.Error as e:
-        logger.error(f"Error connecting to the database: {e}")
-    return conn
-
-
-def get_last_processed_time(conn):
-    """ Get the last processed tweet time from the database """
-
-    cur = conn.cursor()
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS tweets (tweet_time TEXT PRIMARY KEY)")
-    cur.execute("SELECT tweet_time FROM tweets ORDER BY tweet_time DESC LIMIT 1")
-    last_time_row = cur.fetchone()
-    return last_time_row[0] if last_time_row else None
-
-
-def set_last_processed_time(conn, tweet_time):
-    logger.info('Setting the last processed tweet time')
-    """ Update the last processed tweet time in the database """
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tweets (tweet_time) VALUES (?)", (tweet_time,))
-    conn.commit()
+def set_last_processed_time(tweet_time):
+    global last_processed_time
+    last_processed_time = tweet_time
 
 
 def nvctranslator(tweet_text):
     """ Convert tweet text to nvc language """
-    tweet_text = tweet_text.replace(
-        '@nvctranslator', '').replace('\n', '').strip()
+
     if (len(tweet_text) > 0):
         logger.info('Converting tweet text into nvc language')
         try:
-            url = f"https://nvctranslator.com/translate?text={urllib.parse.quote(tweet_text)}"
+            url = "https://nvctranslator.com/post"
+            # data to be sent in the POST request
+            payload = {"text": tweet_text}
+            headers = {"Content-Type": "application/json"}
             response = requests.request(
-                "GET", url)
-        except Exception as e:
-            logging.error("Exception occurred in retrieveTweet")
-            logging.error(str(e))
+                "POST", url, headers=headers, data=json.dumps(payload))
+            response.raise_for_status()
+        except requests.RequestException as e:
+            logger.error(f"Error occurred in nvctranslator: {e}")
             return None
-        if (response.json()[1] == 200):
+
+        if (response.json()[1] == 201):
             logger.info("Text succesfully translated")
-            translatedText = response.json()[0]['translation'].split('\n')[
-                1].split(': ')[1][1:-1]
+            translatedText = response.json()[0]['translation']
+
+            matches = re.findall(r'rephrased_txt: "(.*?)"', translatedText)
+            translatedText = ' '.join(matches)
             return translatedText.strip()
         else:
-            logger.error("unable to complete API get request")
+            logger.error("unable to complete API Post request")
             return None
     else:
-        logger.info("No text provided to translate")
+        logger.warning("No text provided to translate")
         return None
 
 
-def reply_to_tweet(tweet_id, reply_text, client):
+def reply_to_tweet(tweet_id, reply_text):
     # create tweet with reply to specific tweet id
     try:
         # Create a tweet in reply to the specified tweet ID
-        logger.info(f'Replying to tweet {tweet_id}')
-        # Create a tweet in reply to the specified tweet ID
-        status = client.create_tweet(
-            text=str(reply_text), in_reply_to_tweet_id=str(tweet_id))
-        logger.info(f"Successfully replied to tweet {tweet_id}")
+        logger.info('Replying to user')
+        client = tweepy.Client(bearer_token=BEARER_TOKEN,
+                               consumer_key=API_KEY, consumer_secret=API_KEY_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+        status = client.create_tweet(text=str(
+            reply_text), in_reply_to_tweet_id=tweet_id)
+        logger.info(status)
+        logger.info("Successfully replied")
 
-    except tweepy.TweepError as e:
+    except Exception as e:
         # Handle any exceptions that might occur during the tweet creation
         logger.error(f"Error replying to tweet {tweet_id}: {e}")
 
 
 def main():
+
     try:
         logger.info("Twitter bot started")
-        # Database connection
-        db_file = 'tweets.db'
-        conn = create_connection(db_file)
-
-        # Load the last processed tweet's time
-        if (get_last_processed_time(conn) == None):
-            start_time = datetime.utcnow()
-            start_time_str = start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
-            set_last_processed_time(conn, start_time_str)
-        logger.info('Getting the last processed tweet time')
-        last_processed_time = get_last_processed_time(conn)
-        print(last_processed_time)
 
         # Initialize Tweepy Client
-        client = tweepy.Client(bearer_token=BEARER_TOKEN,
-                               consumer_key=API_KEY,
-                               consumer_secret=API_KEY_SECRET,
-                               access_token=ACCESS_TOKEN,
-                               access_token_secret=ACCESS_TOKEN_SECRET,
-                               return_type=dict,
+        client = tweepy.Client(bearer_token=BEARER_TOKEN, return_type=dict,
                                wait_on_rate_limit=True)
 
         # Fetch mentions since the last processed tweet
         logger.info("Fetching latest mentions tweets")
+
         mentions = client.get_users_mentions(
             id='1640149719447109633',
             start_time=last_processed_time,
-            tweet_fields=["created_at", "text", "author_id"],
-            expansions=["author_id"],
+            tweet_fields=["created_at", "author_id", "conversation_id"],
+            expansions=["in_reply_to_user_id", "referenced_tweets.id",
+                        'author_id', 'edit_history_tweet_ids'],
             user_fields=["username"]
         )
-        newest_time = None
+
+        newest_time = get_last_processed_time()
 
         if 'data' in mentions:
             for tweet in mentions['data']:
                 try:
                     # Get tweet details
                     tweet_id = tweet['id']
-                    tweet_text = tweet['text']
                     tweet_created_at = tweet['created_at']
 
-                    # Get user details
-                    # 'includes' contains additional information like user details
-                    author_id = tweet['author_id']
-                    author_details = next(
-                        (user for user in mentions['includes']['users'] if user['id'] == author_id), None)
-                    if author_details:
-                        username = author_details['username']
-                    logger.info(
-                        f"Tweet ID: {tweet_id}, Created at: {tweet_created_at}, Username: {username}, Text: {tweet_text}")
-
-                    # Your code to reply to the tweet
-                    translated_text = nvctranslator(tweet_text=tweet_text)
-                    if (translated_text):
-                        reply_text = f"Here is @{username}’s message in a form of non-violent communication: {translated_text}"
-                    else:
-                        reply_text = f"Hello! You can get your text converted to NVC language by mentioning '@nvctranslator' followed by your message."
-
-                    reply_to_tweet(tweet_id=tweet_id, reply_text=str(
-                        reply_text.strip()), client=client)
-
                     # Update the newest tweet time
-                    newest_time = (datetime.strptime(
+                    created_time = (datetime.strptime(
                         tweet_created_at, '%Y-%m-%dT%H:%M:%S.%fZ')+timedelta(seconds=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-                    if (datetime.strptime(newest_time, '%Y-%m-%dT%H:%M:%SZ') < datetime.strptime(last_processed_time, '%Y-%m-%dT%H:%M:%SZ')):
-                        newest_time = last_processed_time
+                    if (datetime.strptime(newest_time, '%Y-%m-%dT%H:%M:%SZ') < datetime.strptime(created_time, '%Y-%m-%dT%H:%M:%SZ')):
+                        newest_time = created_time
+
+                    tweet_author_id = tweet['author_id']
+                    in_reply_to_tweet_id = None
+                    if ('referenced_tweets' in tweet):
+                        in_reply_to_tweet_id = next(
+                            (ref_tweet['id'] for ref_tweet in tweet['referenced_tweets'] if ref_tweet['type'] == 'replied_to'), None)
+                    if (in_reply_to_tweet_id):
+                        # Get user details
+                        in_reply_to_user_id = tweet['in_reply_to_user_id']
+                        in_reply_to_user_tweet_details = next(
+                            (one_tweet for one_tweet in mentions['includes']['tweets'] if one_tweet['id'] == in_reply_to_tweet_id), None)
+                        if (in_reply_to_user_tweet_details):
+                            in_reply_to_user_text = in_reply_to_user_tweet_details['text']
+                        userdetails_to_reply = next(
+                            (user for user in mentions['includes']['users'] if user['id'] == in_reply_to_user_id), None)
+                        username_to_reply = None
+                        if (userdetails_to_reply):
+                            username_to_reply = userdetails_to_reply['username']
+                        userdetails_who_posted = next(
+                            (user for user in mentions['includes']['users'] if user['id'] == tweet_author_id), None)
+                        username_who_posted = None
+                        if (userdetails_who_posted):
+                            username_who_posted = userdetails_who_posted['username']
+
+                        # Your code to reply to the tweet
+                        translated_text = nvctranslator(
+                            tweet_text=str(in_reply_to_user_text.replace('\n\n', ' ')))
+
+                        if (username_who_posted != username_to_reply):
+                            reply_text = f"Here is @{username_to_reply}’s message in a form of non-violent communication: {translated_text}"
+                            reply_to_tweet(tweet_id=tweet_id,
+                                           reply_text=str(reply_text.strip()))
+                    else:
+                        logger.warning('This tweet is not a reply')
 
                 except Exception as e:
                     logger.error(e)
-
-            # Save the time of the most recent tweet processed
+        # set the last processed time to latest processed tweet time
             if newest_time is not None and newest_time != last_processed_time:
-                set_last_processed_time(conn, newest_time)
+                set_last_processed_time(newest_time)
         else:
             logger.warning('No mentions found')
-        conn.close()
-
-    except Exception as main_exception:
-        logger.error(
-            f"An unexpected error occurred in the main function: {main_exception}")
+    except Exception as e:
+        logger.error(f"error in main function :-{e}")
+        # Save the time of the most recent tweet processed
 
 
 if __name__ == "__main__":
-    WAIT_TIME = 1  # min
+    WAIT_TIME = 1.01  # min
     while 1:
-        try:
-            main()
-            time.sleep(WAIT_TIME * 10)
-        except Exception as outer_exception:
-            logger.error(
-                f"An unexpected error occurred in the main loop: {outer_exception}")
+        main()
+        time.sleep(WAIT_TIME*60)
