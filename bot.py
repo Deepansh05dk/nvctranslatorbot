91590677698342d9495638f6e770a7a27d30e4a7
Deepansh05dk <84671261+Deepansh05dk@users.noreply.github.com>
2023-11-17 03:50:31 +0530

    added async await functionality

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import re
 import tweepy
 from dotenv import load_dotenv
 import os
-import requests
 import logging
 import time
 from datetime import datetime, timedelta
-import json
+import asyncio
+import aiohttp
 
 
 load_dotenv()
@@ -39,32 +39,38 @@ def set_last_processed_time(tweet_time):
     last_processed_time = tweet_time
 
 
-def nvctranslator(tweet_text):
-    """ Convert tweet text to nvc language """
+def show_time(text):
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(text, current_time)
 
-    if (len(tweet_text) > 0):
-        logger.info('Converting tweet text into nvc language')
+
+def chunks(lst, chunk_size):
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
+
+
+async def nvctranslator(tweet_text):
+    """ Convert tweet text to nvc language """
+    if len(tweet_text) > 0:
         try:
             url = "https://nvctranslator.com/post"
-            # data to be sent in the POST request
             payload = {"text": tweet_text}
             headers = {"Content-Type": "application/json"}
-            response = requests.request(
-                "POST", url, headers=headers, data=json.dumps(payload))
-            response.raise_for_status()
-        except requests.RequestException as e:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, json=payload, headers=headers) as response:
+                    if response.status == 200:
+                        data = await response.json()  # Assuming the response is in JSON format
+                        translatedText = data[0].get('translation', '')
+                        matches = re.findall(
+                            r'rephrased_txt: "(.*?)"', translatedText)
+                        return ' '.join(matches).strip()
+                    else:
+                        logger.error(
+                            f"Error with status code: {response.status}")
+                        return None
+        except Exception as e:  # Catching a more general exception
             logger.error(f"Error occurred in nvctranslator: {e}")
-            return None
-
-        if (response.json()[1] == 201):
-            logger.info("Text succesfully translated")
-            translatedText = response.json()[0]['translation']
-
-            matches = re.findall(r'rephrased_txt: "(.*?)"', translatedText)
-            translatedText = ' '.join(matches)
-            return translatedText.strip()
-        else:
-            logger.error("unable to complete API Post request")
             return None
     else:
         logger.warning("No text provided to translate")
@@ -88,8 +94,64 @@ def reply_to_tweet(tweet_id, reply_text):
         logger.error(f"Error replying to tweet {tweet_id}: {e}")
 
 
-def main():
+async def handle_each_tweet(param, index):
+    try:
+        show_time(f"start tweet {index}")
+        # Get tweet details
+        tweet_id = param['tweet']['id']
+        tweet_created_at = param['tweet']['created_at']
 
+        # Update the newest tweet time
+        created_time = (datetime.strptime(
+            tweet_created_at, '%Y-%m-%dT%H:%M:%S.%fZ')+timedelta(seconds=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        # Save the time of the most recent tweet processed
+        if (index == 0):
+            set_last_processed_time(created_time)
+
+        tweet_author_id = param['tweet']['author_id']
+        in_reply_to_tweet_id = None
+        if ('referenced_tweets' in param['tweet']):
+            in_reply_to_tweet_id = next(
+                (ref_tweet['id'] for ref_tweet in param['tweet']['referenced_tweets'] if ref_tweet['type'] == 'replied_to'), None)
+        if (in_reply_to_tweet_id):
+            # Get user details
+            in_reply_to_user_id = param['tweet']['in_reply_to_user_id']
+            in_reply_to_user_tweet_details = next(
+                (one_tweet for one_tweet in param['mentions']['includes']['tweets'] if one_tweet['id'] == in_reply_to_tweet_id), None)
+            if (in_reply_to_user_tweet_details):
+                in_reply_to_user_text = in_reply_to_user_tweet_details['text']
+            userdetails_to_reply = next(
+                (user for user in param['mentions']['includes']['users'] if user['id'] == in_reply_to_user_id), None)
+            username_to_reply = None
+            if (userdetails_to_reply):
+                username_to_reply = userdetails_to_reply['username']
+            userdetails_who_posted = next(
+                (user for user in param['mentions']['includes']['users'] if user['id'] == tweet_author_id), None)
+            username_who_posted = None
+            if (userdetails_who_posted):
+                username_who_posted = userdetails_who_posted['username']
+
+            # Your code to reply to the tweet
+            translated_text = await nvctranslator(
+                tweet_text=str(in_reply_to_user_text.replace('\n\n', ' ')))
+
+            if (username_who_posted == username_to_reply and translated_text == None and len(translated_text) == 0):
+                return
+            reply_text = f"Here is @{username_to_reply}’s message in a form of non-violent communication: {translated_text}"
+            await asyncio.to_thread(reply_to_tweet(tweet_id=tweet_id,
+                                                   reply_text=str(reply_text.strip())))
+        else:
+            logger.warning('This tweet is not a reply')
+
+        show_time(f"end tweet {index}")
+
+    except Exception as e:
+        logger.error(e)
+
+
+async def twitter_bot():
+    global last_processed_time
     try:
         logger.info("Twitter bot started")
 
@@ -99,7 +161,7 @@ def main():
 
         # Fetch mentions since the last processed tweet
         logger.info("Fetching latest mentions tweets")
-
+        show_time("api start")
         mentions = client.get_users_mentions(
             id='1640149719447109633',
             start_time=last_processed_time,
@@ -108,71 +170,30 @@ def main():
                         'author_id', 'edit_history_tweet_ids'],
             user_fields=["username"]
         )
-
-        newest_time = get_last_processed_time()
-
+        show_time("api end")
         if 'data' in mentions:
-            for tweet in mentions['data']:
-                try:
-                    # Get tweet details
-                    tweet_id = tweet['id']
-                    tweet_created_at = tweet['created_at']
-
-                    # Update the newest tweet time
-                    created_time = (datetime.strptime(
-                        tweet_created_at, '%Y-%m-%dT%H:%M:%S.%fZ')+timedelta(seconds=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
-
-                    if (datetime.strptime(newest_time, '%Y-%m-%dT%H:%M:%SZ') < datetime.strptime(created_time, '%Y-%m-%dT%H:%M:%SZ')):
-                        newest_time = created_time
-
-                    tweet_author_id = tweet['author_id']
-                    in_reply_to_tweet_id = None
-                    if ('referenced_tweets' in tweet):
-                        in_reply_to_tweet_id = next(
-                            (ref_tweet['id'] for ref_tweet in tweet['referenced_tweets'] if ref_tweet['type'] == 'replied_to'), None)
-                    if (in_reply_to_tweet_id):
-                        # Get user details
-                        in_reply_to_user_id = tweet['in_reply_to_user_id']
-                        in_reply_to_user_tweet_details = next(
-                            (one_tweet for one_tweet in mentions['includes']['tweets'] if one_tweet['id'] == in_reply_to_tweet_id), None)
-                        if (in_reply_to_user_tweet_details):
-                            in_reply_to_user_text = in_reply_to_user_tweet_details['text']
-                        userdetails_to_reply = next(
-                            (user for user in mentions['includes']['users'] if user['id'] == in_reply_to_user_id), None)
-                        username_to_reply = None
-                        if (userdetails_to_reply):
-                            username_to_reply = userdetails_to_reply['username']
-                        userdetails_who_posted = next(
-                            (user for user in mentions['includes']['users'] if user['id'] == tweet_author_id), None)
-                        username_who_posted = None
-                        if (userdetails_who_posted):
-                            username_who_posted = userdetails_who_posted['username']
-
-                        # Your code to reply to the tweet
-                        translated_text = nvctranslator(
-                            tweet_text=str(in_reply_to_user_text.replace('\n\n', ' ')))
-
-                        if (username_who_posted != username_to_reply):
-                            reply_text = f"Here is @{username_to_reply}’s message in a form of non-violent communication: {translated_text}"
-                            reply_to_tweet(tweet_id=tweet_id,
-                                           reply_text=str(reply_text.strip()))
-                    else:
-                        logger.warning('This tweet is not a reply')
-
-                except Exception as e:
-                    logger.error(e)
-        # set the last processed time to latest processed tweet time
-            if newest_time is not None and newest_time != last_processed_time:
-                set_last_processed_time(newest_time)
+            for batch in chunks(mentions['data'], 30):
+                show_time("api end")
+                show_time(f"start tweets")
+                handle_tweets = [handle_each_tweet(
+                    param={'tweet': tweet, 'mentions': mentions}, index=index) for index, tweet in enumerate(batch)]
+                await asyncio.gather(*handle_tweets)
+                print("new time -", str(last_processed_time))
+                show_time(f"end tweets ")
         else:
             logger.warning('No mentions found')
+
     except Exception as e:
-        logger.error(f"error in main function :-{e}")
-        # Save the time of the most recent tweet processed
+        logger.error(f"error in twitter bot function :-{e}")
+
+
+async def main():
+    WAIT_TIME = 1.01  # min
+    show_time('main start')
+    await asyncio.gather(twitter_bot(), asyncio.sleep(WAIT_TIME*60))
+    show_time('main end')
 
 
 if __name__ == "__main__":
-    WAIT_TIME = 1.01  # min
     while 1:
-        main()
-        time.sleep(WAIT_TIME*60)
+        asyncio.run(main())
